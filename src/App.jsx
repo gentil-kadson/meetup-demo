5d8540796ff7c2131e87d5139a3d87c04788adca
Kadsukee <fernandes.gentilkadsonjac@gmail.com>
2022-02-18 14:56:07 -0300

    added main navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./components/layouts/MainNavigation";
 
 import { Route, Routes } from 'react-router-dom';
  
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" exact element={<AllMeetupsPage />} />
 
