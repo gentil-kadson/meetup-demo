7d64108c38151daac3c05e7033916ce8cb23d08d
Kadsukee <fernandes.gentilkadsonjac@gmail.com>
2022-02-18 12:52:01 -0300

    stablished routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+
+import { Route, Routes } from 'react-router-dom';
+ 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Routes>
+        <Route path="/" exact element={<AllMeetupsPage />} />
+
+        <Route path="/favorites" element={<FavoritesPage />} />
+        
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+      </Routes>
+      
+    </div>
   );
 }
 
