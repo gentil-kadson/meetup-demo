89e420a599e79ad0f5d7e6ffa8b8bc3c0d2314c2
Kadsukee <fernandes.gentilkadsonjac@gmail.com>
2022-02-22 17:19:18 -0300

    implemented some server requests and new meetup creation

--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,5 +1,27 @@
+import { useNavigate } from 'react-router-dom'
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-    return <div>New Meetup Page</div>
+    const navigate = useNavigate()
+
+    function addMeetupHandler(meetupData) {
+        fetch('https://meetup-demo-d5118-default-rtdb.firebaseio.com/meetups.json', {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            navigate('/')
+        })
+    }
+
+    return(
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
+        </section>
+    );
 }
 
 export default NewMeetupPage
